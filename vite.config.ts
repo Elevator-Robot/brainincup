c1e701a1bd0fd2c1aa25714d3993680ce22bff8d
Aaron West <aphexlog@gmail.com>
2025-03-22 10:20:06 -0500

    adding ngrok to allowed hosts and adding css overrides for font color

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['localhost', 'b2b7-136-62-118-167.ngrok-free.app']
+  },
   plugins: [react()],
 })
