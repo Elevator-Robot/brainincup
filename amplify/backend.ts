34419e51048380ef4d837b3253aa4cbc39f2548b
Aaron West <aphexlog@gmail.com>
2025-03-23 10:58:18 -0500

    adding dynamo stream and lambda trigger on Message model

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,7 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { brain } from './functions/brain/resource';
 import { PolicyStatement, Effect } from "aws-cdk-lib/aws-iam"
+import { EventSourceMapping, StartingPosition } from 'aws-cdk-lib/aws-lambda';
 
 const backend = defineBackend({
   auth,
@@ -10,11 +11,35 @@ const backend = defineBackend({
   brain,
 });
 
+const { cfnResources } = backend.data.resources;
+cfnResources.amplifyDynamoDbTables['Message'].streamSpecification = {
+  streamViewType: 'NEW_AND_OLD_IMAGES' as any,
+}
+
+const messageTable = backend.data.resources.tables['Message'];
+
 const brainLambda = backend.brain.resources.lambda;
+
+new EventSourceMapping(backend.stack, 'BrainMessageMapping', {
+  target: brainLambda,
+  eventSourceArn: messageTable.tableStreamArn,
+  startingPosition: StartingPosition.LATEST,
+});
 
 brainLambda.addToRolePolicy(new PolicyStatement({
   actions: ['bedrock:*'],
   resources: ['*'],
+  effect: Effect.ALLOW,
+}));
+
+brainLambda.addToRolePolicy(new PolicyStatement({
+  actions: [
+    'dynamodb:DescribeStream',
+    'dynamodb:GetRecords',
+    'dynamodb:GetShardIterator',
+    'dynamodb:ListStreams',
+  ],
+  resources: [messageTable.tableStreamArn as string],
   effect: Effect.ALLOW,
 }));
 
