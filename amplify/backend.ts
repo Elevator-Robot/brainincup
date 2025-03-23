7e38434064e33b60d538848d0f289a4d20e132b8
Aaron West <aphexlog@gmail.com>
2025-03-23 18:11:36 -0500

    adding lambda layer w py deps

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,13 +3,15 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { brain } from './functions/brain/resource';
 import { PolicyStatement, Effect } from "aws-cdk-lib/aws-iam"
-import { EventSourceMapping, StartingPosition } from 'aws-cdk-lib/aws-lambda';
+import { EventSourceMapping, StartingPosition, LayerVersion, Code, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 const backend = defineBackend({
   auth,
   data,
   brain,
 });
+
+const stack = backend.stack;
 
 const { cfnResources } = backend.data.resources;
 cfnResources.amplifyDynamoDbTables['Message'].streamSpecification = {
@@ -20,7 +22,12 @@ const messageTable = backend.data.resources.tables['Message'];
 
 const brainLambda = backend.brain.resources.lambda;
 
-new EventSourceMapping(backend.stack, 'BrainMessageMapping', {
+// const layer = new LayerVersion(stack, 'BrainDepsLayer', { # TODO: this creates the new layer successfully, but I need to get it working with the lambda function so that it can be attached to the lambda function
+//   code: Code.fromAsset('amplify/functions/brain/layer/python.zip'),
+//   compatibleRuntimes: [Runtime.PYTHON_3_13],
+// });
+
+new EventSourceMapping(stack, 'BrainMessageMapping', {
   target: brainLambda,
   eventSourceArn: messageTable.tableStreamArn,
   startingPosition: StartingPosition.LATEST,
