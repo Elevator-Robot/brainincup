36ae27c8fb140cf50d4ddb88846892ad54cbb630
Aaron West <aphexlog@gmail.com>
2025-03-24 10:52:41 -0500

    got lambda layer uploaded and attached to the fn. TODO: make the fn work using the deps

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,6 +4,7 @@ import { data } from './data/resource';
 import { brain } from './functions/brain/resource';
 import { PolicyStatement, Effect } from "aws-cdk-lib/aws-iam"
 import { EventSourceMapping, StartingPosition, LayerVersion, Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 const backend = defineBackend({
   auth,
@@ -20,12 +21,23 @@ cfnResources.amplifyDynamoDbTables['Message'].streamSpecification = {
 
 const messageTable = backend.data.resources.tables['Message'];
 
-const brainLambda = backend.brain.resources.lambda;
+const brainLambda = backend.brain.resources.lambda as lambda.Function;
 
-// const layer = new LayerVersion(stack, 'BrainDepsLayer', { # TODO: this creates the new layer successfully, but I need to get it working with the lambda function so that it can be attached to the lambda function
-//   code: Code.fromAsset('amplify/functions/brain/layer/python.zip'),
-//   compatibleRuntimes: [Runtime.PYTHON_3_13],
-// });
+const layer = new LayerVersion(stack, 'BrainDepsLayer', {
+  code: Code.fromAsset('amplify/functions/brain/layer', {
+    bundling: {
+      image: Runtime.PYTHON_3_13.bundlingImage,
+      command: [
+        'bash',
+        '-c',
+        'pip install -r requirements.txt -t /asset-output'
+      ],
+    },
+  }),
+  compatibleRuntimes: [Runtime.PYTHON_3_13],
+});
+
+brainLambda.addLayers(layer);
 
 new EventSourceMapping(stack, 'BrainMessageMapping', {
   target: brainLambda,
--- amplify/functions/brain/resource.ts
+++ amplify/functions/brain/resource.ts
@@ -1,37 +1,19 @@
 import { defineFunction } from '@aws-amplify/backend';
-import { Function, Runtime, Code, LayerVersion } from 'aws-cdk-lib/aws-lambda';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import { Duration } from 'aws-cdk-lib';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 
-// Define __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const layerPath = path.join(__dirname, 'layer/python.zip');
-console.log('Layer path:', layerPath);
 
 export const brain = defineFunction((scope) => {
-  // Create a Lambda layer for Python dependencies.
-  const brainDepsLayer = new LayerVersion(scope, 'BrainDepsLayer', {
-    compatibleRuntimes: [Runtime.PYTHON_3_13],
-    code: Code.fromAsset(layerPath),
-  });
-
   return new Function(scope, 'BrainFunction', {
     runtime: Runtime.PYTHON_3_13,
     handler: 'handler.main',
     code: Code.fromAsset(path.join(__dirname, 'src'), {
-      bundling: {
-        local: {
-          tryBundle: (outputDir: string) => {
-            // If your dependencies are now in the layer, you might not need additional bundling.
-            return true;
-          },
-        },
-        image: Runtime.PYTHON_3_13.bundlingImage,
-      },
     }),
-    timeout: Duration.seconds(30),
-    layers: [brainDepsLayer], // Attach the layer here!
+    timeout: Duration.seconds(30)
   });
 });
+
