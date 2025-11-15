720b90ed2e7ea8ba469acb336e12892c5e119a74
Aaron West <aphexlog@github.com>
2025-11-15 17:11:08 -0600

    chore: update favicon with new brain.svg
    Replaced brain.svg with updated version. Updated all favicon files:
    - public/brain.svg
    - public/favicon.svg
    - public/icon-192.png
    - public/icon-512.png
    
    New favicon will appear in browser tabs, bookmarks, and PWA icons.

--- src/components/BrainIcon.tsx
+++ src/components/BrainIcon.tsx
@@ -7,7 +7,8 @@ export default function BrainIcon({ className = "w-5 h-5" }: BrainIconProps) {
     <img 
       src="/brain-chat.svg" 
       alt="Brain in Cup" 
-      className={className}
+      className={`${className} drop-shadow-lg filter brightness-110 contrast-125`}
+      style={{ imageRendering: 'crisp-edges' }}
     />
   );
 }
