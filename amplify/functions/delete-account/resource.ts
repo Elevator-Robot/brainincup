ae9dda23525a2c244bddc3ab58434cff98542d3c
aphexlog <aphexlog@gmail.com>
2026-08-01 16:52:35 -0500

    fix(deploy): co-locate delete-account lambda in data stack

--- amplify/functions/delete-account/resource.ts
+++ amplify/functions/delete-account/resource.ts
@@ -3,4 +3,5 @@ import { defineFunction } from '@aws-amplify/backend';
 export const deleteAccount = defineFunction({
   name: 'delete-account',
   timeoutSeconds: 60,
+  resourceGroupName: 'data',
 });
